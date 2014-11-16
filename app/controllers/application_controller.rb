1cf4d0af8a944c3e39fdb7d5988854dbaa8828c8
LaLueur <y.v.karpenko@gmail.com>
2014-11-16 20:40:46 +0200

    added json format for posts added bootbox-js libraries for alerts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,13 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def update_views
+    cookies[:views] = if cookies[:views].present?
+                        cookies[:views].to_i + 1
+                      else
+                        1
+                      end
+  end
+
   helper_method :current_user
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,28 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :check_auth, only: [:edit, :update, :destroy]
+  before_action :update_views
+
+
 
   # GET /posts
   # GET /posts.json
   def index
     @posts = Post.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @posts, except: :updated_at, :include => {:user => {:only => [:name]}}}
+    end
   end
 
   # GET /posts/1
   # GET /posts/1.json
   def show
+    #@post = Post.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: @post, except: :updated_at, :include => {:user => {:only => [:name]}}}
+    end
   end
 
   # GET /posts/new
@@ -85,4 +97,5 @@ class PostsController < ApplicationController
       redirect_to post_path
     end
   end
+
 end
