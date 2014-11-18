97a4980a50fa70ffd7b1d05c848b3d3530c9a360
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2014-11-18 22:09:14 +0200

    refactored bootbox js call

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :check_auth, only: [:edit, :update, :destroy]
-  before_action :update_views
 
 
 
