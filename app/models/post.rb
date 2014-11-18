1219fd0ea939f7617ed1f584ddcc18919f66f7ae
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2014-11-18 21:15:44 +0200

    added validate presence of user id into posts model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   belongs_to :user
   validates :title, :presence => true, :uniqueness => true, :length => 5..100
   validates_presence_of :body
-  #validates_presence_of :user_id
+  validates_presence_of :user_id
 end
