21561564284ce7ba65dced8929f3b2a478f972ef
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2014-11-25 20:50:27 +0200

    fixed avatar url for dev env

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,6 @@ module ApplicationHelper
   def avatar_url(user)
     default_url = "#{root_url}images/iruby.png"
     gravatar_id = Digest::MD5.hexdigest(user.email).downcase
-    "https://s.gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}?"
+    Rails.env.production? ? "https://s.gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}?": default_url
   end
 end
